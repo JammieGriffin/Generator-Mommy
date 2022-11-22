733bbff4c5309cd6202e2bd3d37b0e9fc2507b0a
taozhiyaoyao <taozhiyaoyao9527@outlook.com>
2022-11-22 21:51:01 +0800

    chore：删除无用注释

--- generators/vue/index.mjs
+++ generators/vue/index.mjs
@@ -1,11 +1,3 @@
-/*
- * @Author: taozhiyaoyao taozhiyaoyao9527@outlook.com
- * @Date: 2022-11-10 17:29:42
- * @LastEditors: taozhiyaoyao taozhiyaoyao9527@outlook.com
- * @LastEditTime: 2022-11-15 22:29:18
- * @FilePath: \generator-mommy\generators\app\index.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import Generator from "yeoman-generator";
 import fs from "fs-extra";
 import pkg from "./package-template.json" assert {type:"json"}
